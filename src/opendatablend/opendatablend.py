8c140c061c10553c0e2a246fff17a659cf28e01a
sscaress <[Your>
2022-02-08 17:39:34 +0530

    Added working Amazon S3 integration. Data file and metadata file existence check to be implemented.

--- src/opendatablend/opendatablend.py
+++ src/opendatablend/opendatablend.py
@@ -2,51 +2,55 @@ import os
 from frictionless import Package
 import requests
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+import boto3
+from botocore.client import ClientError
 
 # Open Data Blend API base URL
 base_url = 'https://packages.opendatablend.io'
 base_url_local_substitution = 'opendatablend'
 
-class Output:   
+class Output:
     def __init__(self, data_file_name, metadata_file_name):
         self.data_file_name = data_file_name
         self.metadata_file_name = metadata_file_name
-    
-    
+
+
 # Get and cache a data file and the dataset metadata
-def get_data(dataset_path, resource_name, base_path='/', access_key='', file_system='local', configuration={}):    
-        
+def get_data(dataset_path, resource_name, base_path='/', access_key='', file_system='local', configuration={}):
+
     # Get the dataset metadata
     dataset = Package(dataset_path)
-    
+
     # Get the data file metadata
     data_file = dataset.get_resource(resource_name)
-    
+
     # Set the fully qualified data file name to mirror the logical folder structure at the server
     data_file_name = base_path + data_file.path.replace(base_url, base_url_local_substitution)
-    
+
     # Cache the file in the specified file system
     output_data_file_name = cache_date_file(data_file, data_file_name, access_key, file_system, configuration)
 
     # Save a copy of the dataset metadata to the specific file system
     if dataset_path.startswith('http'):
         output_metadata_file_name = cache_dataset_metadata(dataset, base_path, file_system, configuration)
-    
+
     output = Output(output_data_file_name, output_metadata_file_name)
-    
+
     # Return the output object which contains the fully qualified file names
     return output
 
 
 def cache_date_file(data_file, data_file_name, access_key, file_system, configuration):
-    
+
     if file_system == "local":
         output_data_file_name = cache_data_file_to_local_file_system(data_file, access_key, data_file_name)
     elif file_system == "azure_blob_storage":
-        output_data_file_name = cache_data_file_to_azure_blob_storage_file_system(data_file, access_key, data_file_name, configuration)           
+        output_data_file_name = cache_data_file_to_azure_blob_storage_file_system(data_file, access_key, data_file_name, configuration)
+    elif file_system == "amazon_s3":
+        output_data_file_name = cache_data_file_to_amazon_s3_file_system(data_file, access_key, data_file_name, configuration)
     else:
         print("No data file could be cached. Please specify a supported file system.")
-    
+
     return output_data_file_name
 
 
@@ -59,26 +63,26 @@ def cache_data_file_to_local_file_system(data_file, access_key, data_file_name):
         except OSError as ex:
             if ex.errno != os.errno.EEXIST:
                 raise
-                
+
     # Only download the data file if it doesn't exist
     if not os.path.exists(data_file_name):
         if access_key != '':
             data = requests.get(data_file.path + '?accesskey=' + access_key, stream=True)
         else:
             data = requests.get(data_file.path + access_key, stream=True)
-        
+
         # Download th data file using a 4 MB chunk size
         local_file = open(data_file_name,'wb')
         for chunk in data.iter_content(chunk_size=4 * 1024 * 1024):
             local_file.write(chunk)
-    
+
         local_file.close()
 
-    return data_file_name  
+    return data_file_name
 
 
 def cache_data_file_to_azure_blob_storage_file_system(data_file, access_key, data_file_name, configuration):
-    
+
     # Get the Azure Blob Storage configurations
     connection_string =  configuration["connection_string"]
     container_name = configuration["container_name"]
@@ -108,10 +112,42 @@ def cache_data_file_to_azure_blob_storage_file_system(data_file, access_key, dat
     return output_data_file_name
 
 
+def cache_data_file_to_amazon_s3_file_system(data_file, access_key, data_file_name, configuration):
+    # Get the aws s3 bucket Storage configurations
+    aws_access_key_id =  configuration["aws_access_key_id"]
+    aws_secret_access_key = configuration["aws_secret_access_key"]
+    bucket_name = configuration["bucket_name"]
+
+    # Create the s3 client resource
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    # Create the bucket if it doesn't exist
+    try:
+        s3_client.head_bucket(Bucket=bucket_name)
+    except ClientError:
+        s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'us-west-1'})
+
+    # Remove the leading slash
+    output_data_file_name = data_file_name.replace("/opendatablend","opendatablend")
+
+    # if not bucket_client.exists():
+    if access_key != '':
+        data = requests.get(data_file.path + '?accesskey=' + access_key, stream=True)
+    else:
+        data = requests.get(data_file.path + access_key, stream=True)
+
+    with data as part:
+        part.raw.decode_content = True
+        conf = boto3.s3.transfer.TransferConfig(multipart_threshold=10000, max_concurrency=4)
+        s3_client.upload_fileobj(part.raw, bucket_name, output_data_file_name, Config=conf)
+
+    return output_data_file_name
+
+
 def cache_dataset_metadata(dataset, base_path, file_system, configuration):
 
     metadata_data_file_snapshot_path = dataset.get('snapshot_path')
-    
+
     # Set the fully qualified metadata file name to mirror the logical folder structure at the server
     metadata_file_name = base_path + metadata_data_file_snapshot_path.replace(base_url, base_url_local_substitution)
 
@@ -119,28 +155,30 @@ def cache_dataset_metadata(dataset, base_path, file_system, configuration):
         output_metadata_file_name = cache_dataset_metadata_to_local_file_system(metadata_data_file_snapshot_path, metadata_file_name)
     elif file_system == "azure_blob_storage":
         output_metadata_file_name = cache_dataset_metadata_to_azure_blob_storage_file_system(metadata_data_file_snapshot_path, metadata_file_name, configuration)
+    elif file_system == "amazon_s3":
+        output_metadata_file_name = cache_dataset_metadata_to_amazon_s3_file_system(metadata_data_file_snapshot_path, metadata_file_name, configuration)
     else:
         print("No metadata file could be cached. Please specify a supported file system.")
-    
+
     return output_metadata_file_name
 
 
 def cache_dataset_metadata_to_local_file_system(metadata_data_file_snapshot_path, metadata_file_name):
-    
+
     # Create the directory for the dataset metadata file if it doesn't exist
     if not os.path.exists(os.path.dirname(metadata_file_name)):
         try:
             os.makedirs(os.path.dirname(metadata_file_name))
         except OSError as ex:
             if ex.errno != os.errno.EEXIST:
-                raise 
-                
+                raise
+
     # Download the dataset metadata file if it doesn't exist
     if not os.path.exists(metadata_file_name):
         data = requests.get(metadata_data_file_snapshot_path)
-        
+
         open(metadata_file_name, 'wb').write(data.content)
-    
+
     # Return the fully qualified metadata file name so it can be used
     return metadata_file_name
 
@@ -171,4 +209,33 @@ def cache_dataset_metadata_to_azure_blob_storage_file_system(metadata_data_file_
         blob_client.upload_blob(data)
 
     # Return the fully qualified metadata file name so it can be used
+    return output_metadata_file_name
+
+
+def cache_dataset_metadata_to_amazon_s3_file_system(metadata_data_file_snapshot_path, metadata_file_name, configuration):
+    # Get the aws s3 bucket Storage configurations
+    aws_access_key_id = configuration["aws_access_key_id"]
+    aws_secret_access_key = configuration["aws_secret_access_key"]
+    bucket_name = configuration["bucket_name"]
+
+    # Create the s3 client
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    # Create the bucket if it doesn't exist
+    try:
+        s3_client.head_bucket(Bucket=bucket_name)
+    except ClientError:
+        s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': 'us-west-1'})
+
+    # Remove the leading slash
+    output_metadata_file_name = metadata_file_name.replace("/opendatablend","opendatablend")
+
+    # if not bucket_client.exists():
+    data = requests.get(metadata_data_file_snapshot_path, stream=True)
+
+    with data as part:
+        part.raw.decode_content = True
+        conf = boto3.s3.transfer.TransferConfig(multipart_threshold=10000, max_concurrency=4)
+        s3_client.upload_fileobj(part.raw, bucket_name, output_metadata_file_name, Config=conf)
+
     return output_metadata_file_name
